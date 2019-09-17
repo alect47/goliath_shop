39e414d0a9a56b164e2a5d04e27e48af2fd949c3
Alec Wells <alec.wells47@gmail.com>
2019-09-16 19:20:27 -0600

    Fix user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:address)}
-    it {should validate_presence_of(:city)}
-    it {should validate_presence_of(:state)}
-    it {should validate_presence_of(:zip)}
-    it {should validate_numericality_of(:zip)}
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
@@ -15,32 +10,31 @@ describe User, type: :model do
 
   describe "relationships" do
     it {should have_many :orders}
+    it {should have_many :addresses}
+    # it {should belong_to :merchant}
   end
 
   describe "roles" do
     it "can be created as a default user" do
-      user = User.create!(name: "alec",
-                        address: "123 Main",
-                        city: "Denver",
-                        state: "CO",
-                        zip: 80222,
-                        email: "623@gamil.com",
-                        password: "password",
-                        role: 0)
+      user = User.create!(  name: "alec",
+        email: "5@gmail.com",
+        password: "password"
+      )
+      user_address = user.addresses.create!(address: '123 Main st', city:'Denver', state:'CO', zip:80219)
+
 
       expect(user.role).to eq("regular_user")
       expect(user.regular_user?).to be_truthy
     end
 
     it "can be created as a merchant_employee" do
-      user = User.create(name: "alec",
-                        address: "123 Main",
-                        city: "Denver",
-                        state: "CO",
-                        zip: 80222,
-                        email: "823@gamil.com",
-                        password: "password",
-                        role: 1)
+      user = User.create!(  name: "alec",
+        email: "5@gmail.com",
+        password: "password",
+        role: 1
+      )
+      user_address = user.addresses.create!(address: '123 Main st', city:'Denver', state:'CO', zip:80219)
+
 
       expect(user.role).to eq("merchant_employee")
       expect(user.merchant_employee?).to be_truthy
@@ -49,14 +43,12 @@ describe User, type: :model do
 
   describe "instance methods" do
     it "can verify that a user has no orders" do
-    user = User.create!(name: "alec",
-                      address: "123 Main",
-                      city: "Denver",
-                      state: "CO",
-                      zip: 80222,
-                      email: "456@gamil.com",
-                      password: "password",
-                      role: 0)
+    user = User.create!(  name: "alec",
+      email: "5@gmail.com",
+      password: "password"
+    )
+    user_address = user.addresses.create!(address: '123 Main st', city:'Denver', state:'CO', zip:80219)
+
     expect(user.no_orders?).to eq(true)
     end
   end
